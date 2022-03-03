a64e64e7b0ed9e3258a602b015f28ce537fb5925
kmaduna <55139448+kmaduna@users.noreply.github.com>
2022-03-03 12:24:27 +0200

    Update contactform.js

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,1 +1,26 @@
-var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+$(document).ready(function() {
+$("#submit").click(function() {
+var name = $("#name").val();
+var email = $("#email").val();
+var message = $("#message").val();
+var contact = $("#contact").val();
+$("#returnmessage").empty(); // To empty previous error/success message.
+// Checking for blank fields.
+if (name == '' || email == '' || contact == '') {
+alert("Please Fill Required Fields");
+} else {
+// Returns successful data submission message when the entered information is stored in database.
+$.post("contact_form.php", {
+name1: name,
+email1: email,
+message1: message,
+contact1: contact
+}, function(data) {
+$("#returnmessage").append(data); // Append returned message to message paragraph.
+if (data == "Your Query has been received, We will contact you soon.") {
+$("#form")[0].reset(); // To reset form fields on success.
+}
+});
+}
+});
+});
