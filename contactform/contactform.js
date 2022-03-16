2d3418125dfb9c843712be508a037a6c62d6385a
kmaduna <55139448+kmaduna@users.noreply.github.com>
2022-03-16 11:35:18 +0200

    Delete contactform.js

--- contactform/contactform.js
+++
@@ -1,8 +0,0 @@
-var config = {
-			apiKey: "AIzaSyAFsR50dYlnVc02YA0OkoqLt9DKFtQbGUI",
-			authDomain: "testtick-29ef4.firebaseapp.com",
-			databaseURL: "https://testtick-29ef4.firebaseio.com",
-			projectId: "testtick-29ef4",
-			storageBucket: "firebase-adminsdk-a63nb@testtick-29ef4.iam.gserviceaccount.com",
-			messagingSenderId: "342030900772"
-		};
